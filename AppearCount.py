cf8570e1540aa641205703f7f30af9a8f63762cf
ValtheCoder <keshia.kdf@gmail.com>
2024-01-23 20:42:24 +0800

    How many times does Emma appeared

--- AppearCount.py
+++ AppearCount.py
@@ -1,3 +1,6 @@
 # Str_x = "Emma is good developer. Emma is a writer"
+str_x = "Emma is good developer. Emma is a writer"
 # Count Function
+appeared = str_x.count("Emma")
 # Print
+print("Emma appeared", appeared, "times")
